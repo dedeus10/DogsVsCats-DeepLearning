41fdb577b2e51a33d9465250d7e788ab9612b6cd
dedeus10 <dedeus.f.l@gmail.com>
2021-01-31 19:01:42 -0300

    Added requirements script

---
+++ requeriments.sh
@@ -0,0 +1,8 @@
+pip3 install numpy
+pip3 install pandas
+pip3 install matplotlib
+pip3 install opencv-python
+sudo apt-get install python3-tk
+pip3 install keras
+pip3 install tensorflow
+pip3 install sklearn
